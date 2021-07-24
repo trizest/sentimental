38f8bb5993ec0683be8222f409f55d982f838207
Joshua Mutch <josh_mutch@hotmail.com>
2021-07-24 11:41:19 +1000

    Delete auth.py

--- scraper/auth.py
+++
@@ -1,6 +0,0 @@
-keys = {
-    'api_key': "1WAb1n7WXDvXzLE56atSx2c2B",
-    'api_secret': "QiOBYavcO7up3fSZ2WU2lDPDbUkIMKTtehuz7a9e8rZpSsIZpg",
-    'bearer_token': "AAAAAAAAAAAAAAAAAAAAAKDARAEAAAAACwk7lxC4ivJu4PHi%2BwAqgY7Dy5E%3DQwYiem4k9izFiyGoIuR5nBGyXF5jjKpZPogrhJw4MrBr3KmXsP",}
-
-
